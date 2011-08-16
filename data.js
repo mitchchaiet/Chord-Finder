9032ead49fd109e359ad26f1347571e45e22b1f5
Levi Beach <levimbeach@gmail.com>
2011-08-15 23:19:33 -0400

    Better fretboard, removed testing canvas, updated json key data, minor style tweeks

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 function Keys(){
 	// private variables
-	var keys = [{"key":"C","signature":"0","major_chords":[{"tonic":"C","sub_dom":"F","dom":"G7"}],"relative_minor_chords":[{"0":"Am","1":"Dm","2":"E7"}],"accidental_chords":[{"dbl_dom":"D7","tri_dom":"A7","dom_rel_minor":"E7"}]},{"key":"G","signature":"1#","major_chords":[{"tonic":"G","sub_dom":"C","dom":"D7"}],"relative_minor_chords":[{"0":"Em","1":"Am","2":"B7"}],"accidental_chords":[{"dbl_dom":"A7","tri_dom":"E7","dom_rel_minor":"B7"}]},{"key":"D","signature":"2#","major_chords":[{"tonic":"D","sub_dom":"G","dom":"A7"}],"relative_minor_chords":[{"0":"Bm","1":"Em","2":"F#7"}],"accidental_chords":[{"dbl_dom":"E7","tri_dom":"B7","dom_rel_minor":"F#7"}]},{"key":"A","signature":"3#","major_chords":[{"tonic":"A","sub_dom":"D","dom":"E7"}],"relative_minor_chords":[{"0":"F#m","1":"Bm","2":"C#7"}],"accidental_chords":[{"dbl_dom":"B7","tri_dom":"F#7","dom_rel_minor":"C#7"}]},{"key":"E","signature":"4#","major_chords":[{"tonic":"E","sub_dom":"A","dom":"B7"}],"relative_minor_chords":[{"0":"C#m","1":"F#m","2":"G#7"}],"accidental_chords":[{"dbl_dom":"F#7","tri_dom":"C#7","dom_rel_minor":"Ab7"}]},{"key":"B","signature":"5#","major_chords":[{"tonic":"B","sub_dom":"E","dom":"F#7"}],"relative_minor_chords":[{"0":"G#m","1":"C#m","2":"D#7"}],"accidental_chords":[{"dbl_dom":"C#7","tri_dom":"Ab7","dom_rel_minor":"Eb7"}]},{"key":"F#","signature":"6#(b)","major_chords":[{"tonic":"F#","sub_dom":"B","dom":"C#7"}],"relative_minor_chords":[{"0":"D#m","1":"G#m","2":"A#7"}],"accidental_chords":[{"dbl_dom":"Ab7","tri_dom":"Eb7","dom_rel_minor":"Bb7"}]},{"key":"Db","signature":"5b","major_chords":[{"tonic":"Db","sub_dom":"Gb","dom":"Ab7"}],"relative_minor_chords":[{"0":"Bbm","1":"Ebm","2":"F7"}],"accidental_chords":[{"dbl_dom":"Eb7","tri_dom":"Bb7","dom_rel_minor":"F7"}]},{"key":"Ab","signature":"4b","major_chords":[{"tonic":"Ab","sub_dom":"Db","dom":"Eb7"}],"relative_minor_chords":[{"0":"Fm","1":"Bbm","2":"C7"}],"accidental_chords":[{"dbl_dom":"Bb7","tri_dom":"F7","dom_rel_minor":"C7"}]},{"key":"Eb","signature":"3b","major_chords":[{"tonic":"Eb","sub_dom":"Ab","dom":"Bb7"}],"relative_minor_chords":[{"0":"Cm","1":"Fm","2":"G7"}],"accidental_chords":[{"dbl_dom":"F7","tri_dom":"C7","dom_rel_minor":"G7"}]},{"key":"Bb","signature":"2b","major_chords":[{"tonic":"Bd","sub_dom":"Eb","dom":"F7"}],"relative_minor_chords":[{"0":"Gm","1":"Cm","2":"D7"}],"accidental_chords":[{"dbl_dom":"C7","tri_dom":"G7","dom_rel_minor":"D7"}]},{"key":"F","signature":"1b","major_chords":[{"tonic":"F","sub_dom":"Bb","dom":"C7"}],"relative_minor_chords":[{"0":"Dm","1":"Gm","2":"A7"}],"accidental_chords":[{"dbl_dom":"G7","tri_dom":"D7","dom_rel_minor":"A7"}]}];
+	var keys = [{"key":"C","signature":"0","major_chords":[{"tonic":[{"chord":"C","modf":"major"}],"sub_dom":[{"chord":"F","modf":"major"}],"dom":[{"chord":"G","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"A","modf":"minor"}],"1":[{"chord":"D","modf":"minor"}],"2":[{"chord":"E","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"D","modf":"7"}],"tri_dom":[{"chord":"A","modf":"7"}],"dom_rel_minor":"E7"}]},{"key":"G","signature":"1#","major_chords":[{"tonic":[{"chord":"G","modf":"major"}],"sub_dom":[{"chord":"C","modf":"major"}],"dom":[{"chord":"D","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"E","modf":"minor"}],"1":[{"chord":"A","modf":"minor"}],"2":[{"chord":"B","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"A","modf":"7"}],"tri_dom":[{"chord":"E","modf":"7"}],"dom_rel_minor":[{"chord":"B","modf":"7"}]}]},{"key":"D","signature":"2#","major_chords":[{"tonic":[{"chord":"D","modf":"major"}],"sub_dom":[{"chord":"G","chord":"major"}],"dom":[{"chord":"A","chord":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"B","modf":"minor"}],"1":[{"chord":"E","modf":"minor"}],"2":[{"chord":"F#","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"E","modf":"7"}],"tri_dom":[{"chord":"B","modf":"7"}],"dom_rel_minor":[{"chord":"F#","modf":"7"}]}]},{"key":"A","signature":"3#","major_chords":[{"tonic":[{"chord":"A","modf":"major"}],"sub_dom":[{"chord":"D","modf":"major"}],"dom":[{"chord":"E","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"F#","modf":"minor"}],"1":[{"chord":"B","modf":"minor"}],"2":[{"chord":"C#","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"B","modf":"7"}],"tri_dom":[{"chord":"F#","modf":"7"}],"dom_rel_minor":[{"chord":"C#","modf":"7"}]}]},{"key":"E","signature":"4#","major_chords":[{"tonic":[{"chord":"E","modf":"major"}],"sub_dom":[{"chord":"A","modf":"major"}],"dom":[{"chord":"B","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"C#","modf":"minor"}],"1":[{"chord":"F#","modf":"minor"}],"2":[{"chord":"G#","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"F#","modf":"7"}],"tri_dom":[{"chord":"C#","modf":"7"}],"dom_rel_minor":[{"chord":"Ab","modf":"7"}]}]},{"key":"B","signature":"5#","major_chords":[{"tonic":[{"chord":"B","modf":"major"}],"sub_dom":[{"chord":"E","modf":"major"}],"dom":[{"chord":"F#","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"G#","modf":"minor"}],"1":[{"chord":"C#","modf":"minor"}],"2":[{"chord":"D#","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"C#","modf":"7"}],"tri_dom":[{"chord":"Ab","modf":"7"}],"dom_rel_minor":[{"chord":"Eb","modf":"7"}]}]},{"key":"F#","signature":"6#(b)","major_chords":[{"tonic":[{"chord":"F#","modf":"major"}],"sub_dom":[{"chord":"B","modf":"major"}],"dom":[{"chord":"C#","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"D#","modf":"minor"}],"1":[{"chord":"G#","modf":"minor"}],"2":[{"chord":"A#","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"Ab","modf":"7"}],"tri_dom":[{"chord":"Eb","modf":"7"}],"dom_rel_minor":[{"chord":"Bb","modf":"7"}]}]},{"key":"Db","signature":"5b","major_chords":[{"tonic":[{"chord":"Db","modf":"major"}],"sub_dom":[{"chord":"Gb","modf":"major"}],"dom":[{"chord":"Ab","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"Bb","modf":"minor"}],"1":[{"chord":"Eb","modf":"major"}],"2":[{"chord":"F","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"Eb","modf":"7"}],"tri_dom":[{"chord":"Bb","modf":"7"}],"dom_rel_minor":[{"chord":"F","modf":"7"}]}]},{"key":"Ab","signature":"4b","major_chords":[{"tonic":[{"chord":"Ab","modf":"major"}],"sub_dom":[{"chord":"Db","modf":"major"}],"dom":[{"chord":"Eb","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"F","modf":"minor"}],"1":[{"chord":"Bb","modf":"minor"}],"2":[{"chord":"C","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"Bb","modf":"7"}],"tri_dom":[{"chord":"F","modf":"7"}],"dom_rel_minor":[{"chord":"C","modf":"7"}]}]},{"key":"Eb","signature":"3b","major_chords":[{"tonic":[{"chord":"Eb","modf":"major"}],"sub_dom":[{"chord":"Ab","modf":"major"}],"dom":[{"chord":"Bb","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"C","modf":"minor"}],"1":[{"chord":"F","modf":"minor"}],"2":[{"chord":"G","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"F","modf":"7"}],"tri_dom":[{"chord":"C","modf":"7"}],"dom_rel_minor":[{"chord":"G","modf":"7"}]}]},{"key":"Bb","signature":"2b","major_chords":[{"tonic":[{"chord":"Bb","modf":"major"}],"sub_dom":[{"chord":"Eb","modf":"major"}],"dom":[{"chord":"F","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"G","modf":"minor"}],"1":[{"chord":"C","modf":"minor"}],"2":[{"chord":"D","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"C","modf":"7"}],"tri_dom":[{"chord":"G","modf":"7"}],"dom_rel_minor":[{"chord":"D","modf":"7"}]}]},{"key":"F","signature":"1b","major_chords":[{"tonic":[{"chord":"F","modf":"major"}],"sub_dom":[{"chord":"Bb","modf":"major"}],"dom":[{"chord":"C","modf":"7"}]}],"relative_minor_chords":[{"0":[{"chord":"D","modf":"minor"}],"1":[{"chord":"G","modf":"minor"}],"2":[{"chord":"A","modf":"7"}]}],"accidental_chords":[{"dbl_dom":[{"chord":"G","modf":"7"}],"tri_dom":[{"chord":"D","modf":"7"}],"dom_rel_minor":[{"chord":"A","modf":"7"}]}]}];
 	
 	var strings = [{"e":"1","a":"2","d":"3","g":"4","b":"5","e2":"6"}];
 
@@ -12,7 +12,7 @@ function Keys(){
 	finger = function(x, y, color) {
 		ctx.fillStyle = color;
 		ctx.beginPath();
-		ctx.arc(x, y, canvas.height()*.03, 0, Math.PI*2, false); 
+		ctx.arc(x, y, canvas.height()*.04, 0, Math.PI*2, false); 
 		ctx.closePath();
 		ctx.fill();
 	}
@@ -33,7 +33,7 @@ function Keys(){
 	    ctx.stroke();
 	}
 	fingerY = function(i) {
-		return ((canvas.height()/4) * i) - ((canvas.height()/4)/2)
+		return ((canvas.height()/8) * i) - ((canvas.height()/8)/2)
 	}
 	
 	// build out the fretboard
@@ -41,24 +41,24 @@ function Keys(){
 		// Get api placement
 		
 		$('.fretboard').show();
-		canvas.attr('height', $('.fretboard').width()*1.618);
+		canvas.attr('height', $('.fretboard').height());
 		canvas.attr('width', $('.fretboard').width());
-		var fretHeight = canvas.height()/4;
-		var stringSpace = canvas.width()/6;
+		var fretHeight = canvas.height()/8;
+		var stringSpace = canvas.width()/7;
 		// print canvas
 		// base 0
 		//fret(0, canvas.width()*.1);
 		
 		// Create horizontal lines
-		for (var i=1; i < 5; i++) {
-			 fret(fretHeight*i, 1,"#e5e5e5");
+		for (var i=1; i < 8; i++) {
+			 fret(fretHeight*i, 1,"#ccc");
 		};
 		// Create vertical lines
-		for (var i=1; i < 6; i++) {
-			 guitarString(stringSpace*i, 6-i,"#666");
+		for (var i=1; i < 7; i++) {
+			 guitarString(stringSpace*i, 4-i,"#666");
 		};
 		//loop through chords
-		var api_url = 'http://pargitaru.co.cc/api/?request=chords&chord='+k.key;
+		var api_url = 'http://pargitaru.co.cc/api/?request=chords&chord='+escape(k.key);
 		console.log(api_url);
 		$.get(api_url, function(data) {
 			console.log(data.chords[0]);
